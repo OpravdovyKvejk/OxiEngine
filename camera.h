7c39a1b6f9bcf40b6cfe118c2d5b5d8627703097
Tomáš Schmied <schmied048@gmail.com>
2018-11-01 10:00:07 +0100

    0.1.7.1 RC2
    Actually fixed the bug this time

--- camera.h
+++ camera.h
@@ -3,56 +3,68 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtx/transform.hpp>
+#include <SDL2/SDL.h>
+#include <iostream>
 
 
 class Camera
 {
     public:
-        Camera(const glm::vec3& pos, float fov, float aspect, float zNear, float zFar)
+        Camera(float aspect = 1280/720, const glm::vec3& pos = glm::vec3(0, 0, 0), bool mouselook = false, float speed = 0.1f, float fov = 80.0f, float zNear = 0.01f, float zFar = 1000.0f)
         {
-            m_perspective = glm::perspective(fov, aspect, zNear, zFar);
+            m_perspective = glm::perspective(glm::radians(fov), aspect, zNear, zFar);
             m_position = pos;
-            m_forward = glm::vec3(0, 0, 1);
+            m_forward = glm::vec3(0, 0, 0);
             m_up = glm::vec3(0, -1, 0);
             m_rotation = glm::vec3(0, 0, 0);
+            m_speed = speed;
+            m_mouselook = mouselook;
         }
 
         inline glm::mat4 GetViewProjection() const
         {
-            glm::mat4 returnMatrix = glm::lookAt(m_position, m_position + m_forward, m_up);
+            //glm::mat4 returnMatrix = glm::lookAt(m_position, m_position + m_forward, m_up);
+            glm::mat4 returnMatrix = glm::lookAt(m_position, m_forward, m_up);
             returnMatrix = m_perspective * returnMatrix;
             return returnMatrix;
+        }
+
+        void MoveForward(glm::vec3 angleOffset)
+        {
+            m_position.x += glm::sin(m_rotation.y + angleOffset.y) * m_speed;//Y
+            m_position.z += glm::cos(m_rotation.y + angleOffset.y) * m_speed;//Y
+            UpdateForward();
         }
 
         void SetRotation(glm::vec3 rotation)
         {
             m_rotation = rotation;
-            if (m_rotation.y != 0)
-            {
-                m_forward.x = glm::sin(m_rotation.y); //Y
-                m_forward.z = glm::cos(m_rotation.y); //Y
-            }
-            if (m_rotation.x != 0)
-            {
-                m_forward.y = glm::sin(m_rotation.x); //X
-                m_forward.z = glm::cos(m_rotation.x); //X
-            }
+            UpdateForward();
         }
 
         void AddRotation(glm::vec3 rotation)
         {
             m_rotation += rotation;
-            if (m_rotation.y != 0)
-            {
-                m_forward.x = glm::sin(m_rotation.y); //Y
-                m_forward.z = glm::cos(m_rotation.y); //Y
-            }
-            if (m_rotation.x != 0)
-            {
-                m_forward.y = glm::sin(m_rotation.x); //X
-                m_forward.z = glm::cos(m_rotation.x); //X
-            }
+            UpdateForward();
         }
+
+        void UpdateForward()
+        {
+            m_forward.x = m_position.x + glm::sin(m_rotation.y); //Yrot
+
+            m_forward.y = m_position.y + glm::sin(m_rotation.x); //Xrot
+
+            m_forward.z = m_position.z + glm::cos(m_rotation.x) * glm::cos(m_rotation.y); //Xrot & Yrot
+        }
+
+        void SetSpeed(float speed)
+        { m_speed = speed; }
+
+        void AddSpeed(float speed)
+        { m_speed += speed; }
+
+        float GetSpeed()
+        { return m_speed; }
 
     private:
         glm::mat4 m_perspective;
@@ -61,6 +73,8 @@ class Camera
         glm::vec3 m_up;
 
         glm::vec3 m_rotation;
+        float m_speed;
+        bool m_mouselook;
 };
 
 #endif // CAMERA_H
--- main.cpp
+++ main.cpp
@@ -13,26 +13,29 @@ int main(int argc, char *argv[])
     std::cout << "Starting engine..." << std::endl;
     std::clock_t engineStart = std::clock();
     SDL_Init(SDL_INIT_EVERYTHING); //Init everything, probably boil down to individual features in the future
-    Display display(640, 480, "OxiEngine x64 v0.1.7"); //Show 'x64'? Engine is x64 only.
+    Display display(640, 480, "OxiEngine x64 v0.1.8");
     double duration = ((std::clock() - engineStart) / (double) CLOCKS_PER_SEC) * 1000;
     std::cout << "Engine started in " << duration << "ms" <<std::endl;
 
     Mesh mesh(".\\res\\monkey3.obj");
     Shader shader(".\\res\\testshader");
     Texture texture(".\\res\\fur.bmp");
-    Camera camera(glm::vec3(0, 0, -4), 70.0f, display.getAspect(), 0.01f, 1000.0f);
+    //Camera camera(glm::vec3(0, 0, -4), 70.0f, display.getAspect(), 0.01f, 1000.0f, 0.1f);
+    Camera camera(display.getAspect());
     Transform transform;
 
     float counter = 0.0f;
     int camRotDir = 0;
     int camRotDirV = 0;
+    int camPosDir = 0;
+    int camPosDirV = 0;
     std::cout << "Tip: Use the arrow keys to look around!" << std::endl;
 
     while (!display.isClosed() )
     {
         display.Clear(0.0f, 0.0f, 0.0f, 1.0f);
 
-        transform.GetRot().x = counter;
+        //transform.GetRot().x = counter;
 
         shader.Bind();
         shader.Update(transform, camera);
@@ -57,6 +60,17 @@ int main(int argc, char *argv[])
                             break;
                         case SDLK_DOWN: camRotDirV = 1;
                             break;
+                        case SDLK_w: camPosDirV = 360;
+                            break;
+                        case SDLK_s: camPosDirV = -180;
+                            break;
+                        case SDLK_a: camPosDir = -90;
+                            break;
+                        case SDLK_d: camPosDir = 90;
+                            break;
+
+                        case SDLK_ESCAPE: display.ShouldClose(true);
+                            break;
                     }
                 break;
                 case SDL_KEYUP:
@@ -70,13 +84,24 @@ int main(int argc, char *argv[])
                             break;
                         case SDLK_DOWN: if (camRotDirV > 0) camRotDirV = 0;
                             break;
+                        case SDLK_w: if (camPosDirV > 0) camPosDirV = 0;
+                            break;
+                        case SDLK_s: if (camPosDirV < 0) camPosDirV = 0;
+                            break;
+                        case SDLK_a: if (camPosDir < 0) camPosDir = 0;
+                            break;
+                        case SDLK_d: if (camPosDir > 0) camPosDir = 0;
+                            break;
                     }
                 break;
                 case SDL_QUIT: display.ShouldClose(true);
                 break;
             }
         }
-        camera.AddRotation(glm::vec3(0.05 * camRotDirV, 0.05 * camRotDir, 0));
+        camera.AddRotation(glm::vec3(4 * glm::radians((float) camRotDirV), 0, 0));
+        camera.AddRotation(glm::vec3(0, 4 * glm::radians((float) camRotDir), 0));
+        if (camPosDir != 0) camera.MoveForward(glm::vec3(0, glm::radians((float) camPosDir), 0));
+        if (camPosDirV != 0) camera.MoveForward(glm::vec3(0, glm::radians((float) camPosDirV), 0));
 
         counter += 0.05f;
     }
